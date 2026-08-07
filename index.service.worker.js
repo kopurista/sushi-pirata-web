53a89745ba2dba796e99dd54b8d8d56ea8f89b99
KOPURISTA <kopurista@gmail.com>
2026-08-07 14:27:09 +0200

    Focos del tutorial, caja de dialogo y cartel de preparacion

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786102624|2114962';
+const CACHE_VERSION = '1786105627|2314259';
 /** @type {string} */
 const CACHE_PREFIX = 'Sushi Pirata-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
