c03f03c4f0354f6bd04632696d0048b5949e3b1b
KOPURISTA <kopurista@gmail.com>
2026-08-15 22:11:27 +0200

    Escuela reordenada: seis niveles de grumetes, ficha en el menu y recetas de pepino

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786756869|6781593';
+const CACHE_VERSION = '1786824685|6344349';
 /** @type {string} */
 const CACHE_PREFIX = 'Sushi Pirata-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
