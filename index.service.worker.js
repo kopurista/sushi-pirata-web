32f0bf666e21873b4541ae94a77f9c29fce7ca3c
KOPURISTA <kopurista@gmail.com>
2026-08-15 03:21:13 +0200

    Tutorial integrado en 10 niveles y jefe Kappa

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786717421|26081474';
+const CACHE_VERSION = '1786756869|6781593';
 /** @type {string} */
 const CACHE_PREFIX = 'Sushi Pirata-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
