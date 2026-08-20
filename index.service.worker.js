b0ae1facdf1c6fe43b5f0db5269a25ac35671952
KOPURISTA <kopurista@gmail.com>
2026-08-20 13:50:05 +0200

    Build web: el lanzamiento recortado a su latigazo

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787225882|32455207';
+const CACHE_VERSION = '1787226585|32276449';
 /** @type {string} */
 const CACHE_PREFIX = 'Sushi Pirata-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
