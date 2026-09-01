0232e51e340d3157320cd1ac9ceddc00d6d50dd4
KOPURISTA <kopurista@gmail.com>
2026-09-01 20:14:19 +0200

    Build web 2026-09-01 20:14

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788254298|14902191';
+const CACHE_VERSION = '1788286456|7328202';
 /** @type {string} */
 const CACHE_PREFIX = 'Sushi Pirata-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
