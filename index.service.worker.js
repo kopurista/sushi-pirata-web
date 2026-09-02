a68f13acc438c0e5be8230760000839d914e6000
KOPURISTA <kopurista@gmail.com>
2026-09-02 21:23:43 +0200

    Repaso del 2 de septiembre: 81 hallazgos aplicados

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788290606|3416595';
+const CACHE_VERSION = '1788377017|12848038';
 /** @type {string} */
 const CACHE_PREFIX = 'Sushi Pirata-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
